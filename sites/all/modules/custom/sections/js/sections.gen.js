f072a4dcacb1e94bf50e006c9f377450b7d6ee8a
Alban Bailly <alban@zivtech.com>
2014-07-27 05:49:56 +0000

    syntax modules and config

--- sites/all/modules/custom/sections/js/sections.gen.js
+++ sites/all/modules/custom/sections/js/sections.gen.js
@@ -50,9 +50,12 @@ sectionsControllers
     })();
   });
 }])
-.controller('articlesCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
+.controller('articlesCtrl', ['$scope', '$routeParams', '$http', '$sce',
+  function($scope, $routeParams, $http, $sce) {
   $http.get('/json/' + $routeParams.tid + '/articles').success(function(result) {
+    $scope.renderHtml = function (htmlCode) {
+      return $sce.trustAsHtml(htmlCode);
+    };
     $scope.articles = (function () {
       return result.node;
     })();
